271101778d18a67f172da3aa8fd547f43ce5fccd
lapinRagnar <lapinragnar@gmail.com>
2022-11-02 08:13:29 +0100

    5 - cree 2 composants QuizHeader et Question
    passer les datas en props
    et afficher dans une iteration v-for
    les données

---
+++ src/components/Question.vue
@@ -0,0 +1,64 @@
+<template>
+
+  <div class="question-container">
+    <h1 class="question">  {{ question.text }} </h1>
+  </div>
+  <div class="options-container">
+    <div class="option" v-for="(option, index) in question.options" :key="index">
+      <p class="option-label"> {{ option.label }} </p>
+      <div class="option-value">
+        <p> {{ option.text }} </p>
+      </div>
+    </div>
+  </div>      
+
+</template>
+
+<script setup>
+  import { defineProps } from 'vue'
+
+  const {question} = defineProps(['question'])
+
+</script>
+
+<style lang="scss" scoped>
+
+
+  .question-container{
+    margin-top: 20px;
+
+    .question {
+      font-size: 40px;
+      margin-bottom: 20px;    
+    }
+  }
+
+  .options-container {
+    
+    .option {
+      display: flex;
+      margin-bottom: 20px;
+      cursor: pointer;
+
+      .option-label {
+        background-color: bisque;
+        width: 60px;
+        height: 60px;
+        font-size: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: brown;
+
+      }
+
+      .option-value {
+        background-color: rgb(149, 121, 121);
+        width: 100%;
+        font-size: 30px;
+        padding: 0px 20px;
+        color:rgb(52, 58, 46);
+      }
+    }
+  }
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,12 +1,26 @@
-<template>
+<template>  
   <div>
-    quiz
+
+    <QuizHeader />
+
+    <div>
+      <Question :question="quiz.questions[currentQuestionIndex]" />
+    </div>
+
   </div>
 </template>
 
 <script setup>
+  import Question from '@/components/Question.vue';
+  import QuizHeader from '@/components/QuizHeader.vue'
+  import { ref } from 'vue';
+  import { useRoute } from 'vue-router'
+  import quizes from '@/data/quize.json'
+
+  const route = useRoute()
+  const quizId = parseInt(route.params.id)
+  const quiz = quizes.find(q => q.id === quizId)
+  const currentQuestionIndex = ref(0)
 
 </script>
-<style lang="scss">
-  
-</style>
+
